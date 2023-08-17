d51b99500be2b116e822da6dac5153f15fed030b
Josué Xicotencatl Avalos <55120403+JosueXA@users.noreply.github.com>
2023-08-16 19:26:00 -0600

    Update server.js use port

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT ?? 1234;
 
     // Middlewares
     this.middlewares();
